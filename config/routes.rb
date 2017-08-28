114e6b2095c2ef2e1bdfe1396dfcfb3b667d4a60
shomy <sksks0607@gmail.com>
2017-08-28 22:39:14 +0900

    メール認証機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
   Rails.application.routes.draw do
 
   root 'top#index'
-  mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
@@ -38,5 +36,8 @@
     resources :messages
   end
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
