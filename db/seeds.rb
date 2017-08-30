332285ff61248446f2a4f8c8ed7ae54c891923e1
shomy <sksks0607@gmail.com>
2017-08-30 18:05:33 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,37 @@
 
 
 1.times do |n|
-    user_id = 1
+    id = 200
+    email = Faker::Internet.email
+    password = "password"
+    name = Faker::Name.name
+    User.create!(email:email,
+    password:password,
+    password_confirmation:password,
+    name:name,
+    id:id,
+    )
+end
+
+1.times do |n|
+    user_id = 200
+    id= 100
     title = "db:seedの実験"
     content = "db:seedの実験"
     Topic.create!(user_id:user_id,
+    id:id,
     title:title,
     content:content,
     )
 end
+
+1.times do |n|
+    topic_id = 100
+    user_id = 200
+    content = "db:seedの実験"
+    Comment.create!(user_id:user_id,
+    topic_id:topic_id,
+    content:content,
+    )
+end
+
